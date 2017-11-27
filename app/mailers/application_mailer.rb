4492cc2e2133121d760d8b4123a01090272a9400
Winn Jewett <winn@oxbowlabs.com>
2017-11-27 15:47:39 -0700

    Updated mailer name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'Secret Santa Creator <nate+santa@oxbowlabs.com>'
+  default from: 'Secret Santa <nate+santa@oxbowlabs.com>'
   layout 'mailer'
 end
